0e969cba55bcfc73f7bba574c1ceb9fc9c06b23e
Dreamkaman <riabushko@gmail.com>
2023-05-24 17:08:01 +0300

    Added corrections to Header, Navigator

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,13 @@
 import Logo from "./components/Logo";
 import Navigator from "./components/Navigator";
 
-// eslint-disable-next-line no-unused-vars
 import s from './Header.module.css';
 
 const Header = () => {
-  return <header>
+  return <header className={s}>
     <Logo />
     <Navigator />
   </header>;
-};
+}
 
 export default Header;
---
+++ src/components/Header/components/Navigator/Navigator.tsx
@@ -0,0 +1,23 @@
+import GetSvg from 'shared/components/GetSvg';
+import NicButton from './components/NicButton'
+
+import s from './Navigator.module.css';
+
+const Navigator = () => {
+    return <nav className={s.navigation}>
+        <div className={s.navigationFlex}>
+            <div className={s.navWrapper}>
+                <GetSvg name={'slimMomTxt'} className={'logoTxt'} />
+                <ul className={s.menu}>
+                    <li className={s.menuItem}>Log in</li>
+                    <li className={s.menuItem}>Registration</li>
+                    <li className={s.menuItem}>Dairy</li>
+                    <li className={s.menuItem}>Calculator</li>
+                </ul>
+            </div>
+            <NicButton />
+        </div>
+    </nav>
+}
+
+export default Navigator;
