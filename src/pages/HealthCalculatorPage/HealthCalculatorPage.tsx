4fc927e02e69c1b79eacb394ac5d43e1a9c62c06
Dreamkaman <riabushko@gmail.com>
2023-05-29 17:38:33 +0300

    Next

--- src/pages/HealthCalculatorPage/HealthCalculatorPage.tsx
+++ src/pages/HealthCalculatorPage/HealthCalculatorPage.tsx
@@ -3,6 +3,7 @@ import CalculateCaloriesForm from 'components/CalculateCaloriesForm';
 import s from './HealthCalculatorPage.module.css';
 
 const HealthCalculatorPage = () => {
+
     return <section className={s.calculatorBg}>
         <div className='container'>
             <CalculateCaloriesForm />
