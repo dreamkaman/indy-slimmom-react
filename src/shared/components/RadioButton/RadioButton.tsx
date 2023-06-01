a1e5783284c9747a1c4b9d06f9e46e988eb6b0bf
Dreamkaman <riabushko@gmail.com>
2023-06-02 01:00:58 +0300

    Added Radiobutton

--- src/shared/components/RadioButton/RadioButton.tsx
+++ src/shared/components/RadioButton/RadioButton.tsx
@@ -1,5 +1,21 @@
+import s from './RadioButton.module.css';
+
+
 const RadioButton = () => {
-    return <></>
+    return <div className={s.radioBtnWrapper}>
+        <label htmlFor="group1">1
+            <input type="radio" name="bloodGroup" id="group1" />
+        </label>
+        <label htmlFor="group2">2
+            <input type="radio" name="bloodGroup" id="group2" />
+        </label>
+        <label htmlFor="group3">3
+            <input type="radio" name="bloodGroup" id="group3" />
+        </label>
+        <label htmlFor="group4">4
+            <input type="radio" name="bloodGroup" id="group4" />
+        </label>
+    </div>
 }
 
 export default RadioButton;
