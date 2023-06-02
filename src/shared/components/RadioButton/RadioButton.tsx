70f89a0076600ed70717c7bda244bd8f7d3ac559
Dreamkaman <riabushko@gmail.com>
2023-06-02 12:53:33 +0300

    Added corrections to LabelInput. Added styles for RadioButton.

--- src/shared/components/RadioButton/RadioButton.tsx
+++ src/shared/components/RadioButton/RadioButton.tsx
@@ -2,20 +2,28 @@ import s from './RadioButton.module.css';
 
 
 const RadioButton = () => {
-    return <div className={s.radioBtnWrapper}>
-        <label htmlFor="group1">1
-            <input type="radio" name="bloodGroup" id="group1" />
-        </label>
-        <label htmlFor="group2">2
-            <input type="radio" name="bloodGroup" id="group2" />
-        </label>
-        <label htmlFor="group3">3
-            <input type="radio" name="bloodGroup" id="group3" />
-        </label>
-        <label htmlFor="group4">4
-            <input type="radio" name="bloodGroup" id="group4" />
-        </label>
-    </div>
+    return <>
+        <h3 className={s.radioBtnTitle}>Blood type*</h3>
+        <ul className={s.radioBtnWrapper}>
+            <li>
+                <input type="radio" name="bloodGroup" id="group1" defaultChecked />
+                <label htmlFor="group1">1</label>
+            </li>
+            <li>
+                <input type="radio" name="bloodGroup" id="group2" />
+                <label htmlFor="group2">2</label>
+            </li>
+            <li>
+                <input type="radio" name="bloodGroup" id="group3" />
+                <label htmlFor="group3">3</label>
+            </li>
+            <li>
+                <input type="radio" name="bloodGroup" id="group4" />
+                <label htmlFor="group4">4</label>
+            </li>
+        </ul>
+    </>
+
 }
 
 export default RadioButton;
