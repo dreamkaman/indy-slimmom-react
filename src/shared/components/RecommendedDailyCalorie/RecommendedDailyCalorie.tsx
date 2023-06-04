7638aa95151723d45ab7351ee6c9d2c0a44dd05f
Dreamkaman <riabushko@gmail.com>
2023-06-04 22:35:40 +0300

    Corrected ModalWindow. Added RecommendedDailyCalorie.

---
+++ src/shared/components/RecommendedDailyCalorie/RecommendedDailyCalorie.tsx
@@ -0,0 +1,45 @@
+import Button from '../Button/Button';
+
+import s from './RecommendedDailyCalorie.module.css';
+
+const RecommendedDailyCalorie = () => {
+    const count = 2800;
+    const foodList = [
+        {
+            id: 1,
+            name: 'Red meat'
+        },
+        {
+            id: 2,
+            name: 'Cheese'
+        },
+        {
+            id: 3,
+            name: 'Milk'
+        },
+        {
+            id: 4,
+            name: 'Flour products'
+        },
+        {
+            id: 5,
+            name: 'Tomato'
+        },
+        {
+            id: 6,
+            name: 'Paper'
+        },
+    ];
+
+    return <form className={s.recommendedDailyCalorieForm}>
+        <h3 className={s.formTitle}>Your recommended daily <br />calorie intake is</h3>
+        <p className={s.calorieTxt}>{count} <span>kcal</span></p>
+        <h4 className={s.formSubtitle}>Foods you should not eat</h4>
+        {foodList.length && <ol className={s.foodList}>
+            {foodList.map(foodItem => <li key={foodItem.id}>{foodItem.name}</li>)}
+        </ol>}
+        <Button type='button' name='Start losing weight' className={`${s.startLosingWeightBtn} buttonRectangleActive`} />
+    </form>
+}
+
+export default RecommendedDailyCalorie;
