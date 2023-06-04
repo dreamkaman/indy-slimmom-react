dff85b42e37623083b95f7afee2b5e112146ff4b
Dreamkaman <riabushko@gmail.com>
2023-06-04 13:29:09 +0300

    Corrected styles of Button. Added LoginUserForm.

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,13 @@
+import LoginUserForm from "components/LoginUserForm";
+
+import s from './LoginPage.module.css';
+
 const LoginPage = () => {
-    return <section></section>
+    return <section className="container">
+        <div className={s.loginFormWrapper}>
+            <LoginUserForm />
+        </div>
+    </section>
 }
 
 export default LoginPage;
