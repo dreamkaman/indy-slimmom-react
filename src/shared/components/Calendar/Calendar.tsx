abd492b8a95f2131fbe06170dc034495196a54f1
Dreamkaman <riabushko@gmail.com>
2023-06-06 23:29:48 +0300

    Next

--- src/shared/components/Calendar/Calendar.tsx
+++ src/shared/components/Calendar/Calendar.tsx
@@ -1,12 +1,14 @@
 import GetSvg from "../GetSvg";
 
+import { dateFormatter } from "shared/tools/dateFormatter/dateFormatter";
+
 import s from "./Calendar.module.css";
 
 const Calendar = () => {
-    const date = new Date().toDateString();
+    const date = new Date();
 
     return <>
-        <p className={s.dateText}>{date}</p>
+        <p className={s.dateText}>{dateFormatter(date)}</p>
         <GetSvg name="calendarBtn" className={s.calendarIcon} />
     </>
 }
--- src/shared/tools/dateFormatter/dateFormatter.ts
+++ src/shared/tools/dateFormatter/dateFormatter.ts
@@ -1,3 +1,8 @@
-export const dateFormatter = (date) => {
-    return date;
+type DateFormatterFunction = (date: Date) => string;
+
+export const dateFormatter: DateFormatterFunction = (date) => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day > 9 ? day : `0${day}`}.${month > 9 ? month : `0${month}`}.${year}`;
 }
