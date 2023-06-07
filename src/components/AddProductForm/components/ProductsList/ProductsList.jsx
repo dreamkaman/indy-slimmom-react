f960daf879ec1630e91a61fa4995ffb23634124f
Dreamkaman <riabushko@gmail.com>
2023-06-07 19:27:27 +0300

    Next

--- src/components/AddProductForm/components/ProductsList/ProductsList.jsx
+++ src/components/AddProductForm/components/ProductsList/ProductsList.jsx
@@ -1,23 +1,32 @@
-// import OutputText from "./components/OutputText";
+import Button from 'shared/components/Button/Button';
+import OutputText from './components/OutputText';
+
+import s from './ProductsList.module.css';
+import GetSvg from 'shared/components/GetSvg/GetSvg';
 
 const ProductsList = () => {
     const productsList = [
         {
             id: 1,
             productName: 'Cheese',
-            weight: 150
+            productWeight: 150,
+            productCalories: 750,
         },
         {
             id: 2,
             productName: 'Milk',
-            weight: 250
+            productWeight: 250,
+            productCalories: 900,
         }
     ];
 
-    return <ul>
-        {productsList.map((product) => {
-            return <li key={product.id}>{product.productName}</li>
-        })}
+    return <ul className={s.productsList}>
+        {productsList.map((product) => <li key={product.id} className={s.productsListItem}>
+            <OutputText text={product.productName} className={s.outputTextProductName} />
+            <OutputText text={product.productWeight} className={s.outputTextProductWeight} />
+            <OutputText text={product.productCalories} className={s.outputTextProductCalories} />
+            <Button className={s.closeBtn}><GetSvg name='closeBtn' className={s.closeIcon} /></Button>
+        </li>)}
 
     </ul>
 }
