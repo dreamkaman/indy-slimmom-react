003db522fa058520ff0fc50a9aa64d220c952d96
Dreamkaman <riabushko@gmail.com>
2023-06-15 22:29:57 +0300

    Added MUI, DateCalendar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 import Header from 'components/Header';
 
 import AppRouts from 'routes/AppRouts';
 
 function App() {
   return (
-    <>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Header />
       <AppRouts />
-    </>
+    </LocalizationProvider>
   );
 }
 
