9417734fb39502ae93096dec39cee35f118da454
Dreamkaman <riabushko@gmail.com>
2023-06-23 13:10:14 +0300

    Added corrections to the react hook form

--- src/shared/reactHookFormRules/index.ts
+++ src/shared/reactHookFormRules/index.ts
@@ -1,16 +1,32 @@
 export const nameRules = {
-    required: true,
-    minLength: 2,
-    maxLength: 8
+    required: 'The value of the "Name" field is required!',
+    minLength: {
+        value: 2,
+        message: 'The minimum length of the "Name" field must be 2 characters!'
+    }
+    ,
+    maxLength: {
+        value: 8,
+        message: 'The maximum length of the "Name" field must be 8 characters!'
+    }
 }
 
 export const emailRules = {
-    required: true,
-    pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+    required: 'The value of the "Email" field is required!',
+    pattern: {
+        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+        message: 'The email you entered does not meet the email requirements!'
+    }
 }
 
 export const passwordRules = {
-    required: true,
-    minLength: 8,
-    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,20}$/
+    required: 'The value of the "Password" field is required!',
+    minLength: {
+        value: 8,
+        message: 'The minimum length of the "Password" field must be 8 characters!'
+    },
+    pattern: {
+        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$@!%&*?])[A-Za-z\d#$@!%&*?]{8,20}$/,
+        message: 'The password you entered does not meet the password requirements! Password must contain at least 1 capital letter, 1 number and 1 special character!'
+    }
 }
--- src/shared/tools/validation/index.ts
+++ src/shared/tools/validation/index.ts
@@ -6,26 +6,24 @@ export const validation = (errors: FieldErrors<FieldValues>) => {
     const errorKeys = Object.keys(errors);
     if (errorKeys.length) {
         errorKeys.forEach(key => {
-            let message = '';
+            let message: string | FieldValues = '';
+            console.log(errors[key].message);
 
             switch (errors[key].type) {
                 case 'required':
-                    message = `The value of the field "${key.toUpperCase()}" is required!`
+                    message = errors[key].message || `The value of the field "${key.toUpperCase()}" is required!`;
                     break;
                 case 'minLength':
-                    message = `The value of the field "${key.toUpperCase()}" is too short!`;
+                    message = errors[key].message || `The value of the field "${key.toUpperCase()}" is too short!`;
                     break;
                 case 'maxLength':
-                    message = `The value of the field "${key.toUpperCase()}" is too long!`;
+                    message = errors[key].message || `The value of the field "${key.toUpperCase()}" is too long!`;
                     break;
                 case 'pattern':
-                    message = `The value of the field "${key.toUpperCase()} is wrong!"`;
-                    if (key === 'password') {
-                        message += ` The password should includes at least 1 capital latter, 1 number and 1 special symbol!`;
-                    }
+                    message = errors[key].message || `The value of the field "${key.toUpperCase()} is wrong!"`;
                     break;
                 default:
-                    message = `The form field "${key}" doesn't meet requirements!`;
+                    message = errors[key].message || `The form field "${key}" doesn't meet requirements!`;
                     break;
             }
 
